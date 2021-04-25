4df9883c18ee331429f03a0d1c56e87b4f6c723b
Jason <jharvey0231@gmail.com>
2021-04-25 14:38:43 -0400

    reviewed

--- HealthCalculator.py
+++ HealthCalculator.py
@@ -1,6 +1,7 @@
 # Alex Harvey, 4/11/2021, file name HealthCalculator.py
 # This will ask user for information to use to calculate BMI
 # Ask user for input
+from decimal import Decimal
 print ('Please enter the following values for the user...')
 height = int(input('Height in inches: '))
 weight = int(input('Weight in pounds: '))
@@ -8,6 +9,7 @@ age = int(input('Current age: '))
 restingHeart = int(input('Resting hart rate: ')) 
 convertHeight = height * .0254
 convertWeight = weight * .453592
+
 
 
 # show the results displaying BMI and what category they are in
@@ -26,4 +28,12 @@ elif bmi >= 30:
     
 print('Exercise Intensity Heart Rates:')
 print('Intensity {: >15}' .format ('Max Heart Rate'))
-int(print(((220 - age)- restingHeart) * 50) + restingHeart
+inteRange=Decimal(.96)
+intes= Decimal(.50)
+
+for inteRange in range(50,96,5):
+    
+    max_heart_rate = ((220-age-restingHeart)*intes+restingHeart)
+    print(f'{intes: .2f}{" ":>10}{max_heart_rate: .0f}')
+    #print(int(((220 - age)- restingHeart) * intes) + restingHeart)
+    intes += Decimal(.05)
